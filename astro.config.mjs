30a69928e41503795dff70b31c9efb65c587f508
Moxie <astronomicalgamer5@gmail.com>
2024-07-18 22:50:00 +0000

    added astro-icon added color pallet setup typescript import shortcuts added navbar header started recipe search bar component added 404 page added empty pages for browsing setup remote fetch of markdown from db

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,39 +6,30 @@ import playformCompress from "@playform/compress";
 import purgecss from "astro-purgecss";
 import db from "@astrojs/db";
 import node from "@astrojs/node";
-
 import markdownIntegration from "@astropub/md";
+import devtoolBreakpoints from "astro-devtool-breakpoints";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    markdownIntegration(),
-    tailwind({
-      applyBaseStyles: true,
-    }),
-    playformInline(),
-    purgecss({
-      fontFace: true,
-      keyframes: false,
-      rejected: true,
-      variables: true,
-      extractors: [
-        {
-          extractor: (content) =>
-            content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [],
-          extensions: ["astro", "html"],
-        },
-      ],
-    }),
-    playformCompress(),
-    compressor({
-      gzip: false,
-      brotli: true,
-    }),
-    db(),
-  ],
+  integrations: [markdownIntegration(), tailwind({
+    applyBaseStyles: true
+  }), playformInline(), purgecss({
+    fontFace: true,
+    keyframes: false,
+    rejected: true,
+    variables: true,
+    extractors: [{
+      extractor: content => content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [],
+      extensions: ["astro", "html"]
+    }]
+  }), playformCompress(), compressor({
+    gzip: false,
+    brotli: true
+  }), db(), devtoolBreakpoints(), icon()],
   output: "server",
   adapter: node({
-    mode: "standalone",
-  }),
+    mode: "standalone"
+  })
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        kitchen: {
+          bg: "#f4f1de",
+          fg: "#3d405b",
+          primary: "#81b29a",
+          secondary: "#e07a5f",
+          accent: "#f2cc8f",
+        },
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
