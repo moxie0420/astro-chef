f8f8494bc2f0688e5f2356ef71e3c18a72243dac
Moxie Benavides <moxie@moxiege.com>
2024-08-19 18:42:45 -0500

    update schema start new ingredient holder

--- schema.ts
+++ schema.ts
@@ -1,9 +1,25 @@
 import { sqliteTable, text, integer, real } from "drizzle-orm/sqlite-core";
+import { sql } from "drizzle-orm";
+
+export const Units = [
+  "teaspoon",
+  "tablespoon",
+  "cup",
+  "ounce",
+  "fluid cup",
+  "fluid ounce",
+  "pint",
+  "quart",
+  "gallon",
+  "kilograms",
+  "grams",
+  "miligram",
+];
 
 export const ingredients = sqliteTable("ingredients", {
   Id: integer("Id").primaryKey(),
   Recipe: integer("Recipe"),
-  Title: text("Title"),
+  Name: text("Title"),
   Amount: real("Amount"),
   Unit: text("Unit", {
     enum: [
@@ -27,7 +43,10 @@ export const recipes = sqliteTable("recipes", {
   Id: integer("Id").primaryKey(),
   Title: text("Title"),
   Body: text("Body"),
-  Ingredient_ids: integer("Ingredient_ids"),
+  Ingredient_ids: text("Ingredient_ids", { mode: "json" })
+    .notNull()
+    .$type<string[]>()
+    .default(sql`'[]'`),
   PrepTime: integer("PrepTime"),
   CookTime: integer("CookTime"),
 });
