d046ccae9e3abbb700926d9fda469a548929c389
Moxie Benavides <moxie@moxiege.com>
2024-12-29 19:50:56 -0600

    update pnpm deps

--- package.nix
+++ package.nix
@@ -15,7 +15,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   pnpmDeps = pnpm.fetchDeps {
     inherit (finalAttrs) pname version src;
-    hash = "sha256-sVvl0OW7vjnH0Hc0MiASX9XS6EF+936bigkEvb9ado4=";
+    hash = "sha256-atb487T1rJMcuyF/QAubRMoceogQSkwV5F/Fn/eu1U4=";
   };
 
   ASTRO_TELEMETRY_DISABLED = 1;
